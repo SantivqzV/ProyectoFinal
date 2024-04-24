704b6ee34c3b43b12f38e6cb83c2ad7bd3564d12
Elias Serrano <eliasjosf26@gmail.com>
2024-04-23 18:18:41 -0600

    cambie route del put escena

--- api/routes/escenas.routes.js
+++ api/routes/escenas.routes.js
@@ -8,7 +8,7 @@ const router = Router();
 
 router.get("/escena/:id_trabajador", getEscena);
 
-router.put("/escena/:id_trabajador/:escena", putEscena);
+router.put("/put_escena/:id_trabajador/:escena", putEscena);
 
 
 export default router;
