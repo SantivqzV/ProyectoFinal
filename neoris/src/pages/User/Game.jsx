12b25ab0d10935db59d20707f742b9218027c683
AylenA <88845840+AylenA@users.noreply.github.com>
2024-04-26 18:40:11 -0600

    formato juego

--- neoris/src/pages/User/Game.jsx
+++ neoris/src/pages/User/Game.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
-const game = () => {
+const Game = () => {
   return (
-    <div>game</div>
-  )
-}
+    <div>
+      <h1>NEORIS QUEST</h1>
+      <iframe
+        title="Parallax Game"
+        src="https://itch.io/embed-upload/9972479?color=333333"
+        allowFullScreen
+        width="720"
+        height="450"
+        frameBorder="1"
+      >
+        <p>Your browser does not support iframes.</p>
+        <a href="https://aylenaf.itch.io/parallax">Play Parallax on itch.io</a>
+      </iframe>
+    </div>
+  );
+};
 
-export default game
+
+export default Game
