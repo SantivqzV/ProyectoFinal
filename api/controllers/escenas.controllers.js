f75ccf8a54a10fa26b66fcf477e1a23404a2c70f
Elias Serrano <eliasjosf26@gmail.com>
2024-04-29 11:59:14 -0600

    put_escena tiene nueva funcionalidad de poner el siguiente curso que toca

--- api/controllers/escenas.controllers.js
+++ api/controllers/escenas.controllers.js
@@ -4,27 +4,27 @@ export const putCertificado = async (req,res) =>{
   try {
 
     const { data, error } = await supabase
-    .from('escena_dummy')
+    .from('progreso_usuario_juego')
     .update({ "documento": true })
-    .eq('id_trabajador', req.params.id_trabajador)
+    .eq('id_usuario', req.params.id_usuario)
     .select()  
     
+    res.json(data)
   } catch (error) {
     // Catch any unexpected errors and respond with a 500 status and the error message
     res.status(500).json({ error: error.message });
   }
 }
 
-
 export const getEscena = async (req, res) => {
   try {
     // Fetch the curso with the specified ID from the 'Cursos' table
-    console.log('ID parameter:', req.params.id_trabajador); // Log the ID parameter
+    console.log('ID parameter:', req.params.id_usuario); // Log the ID parameter
 
     const { data: escena, error } = await supabase
-      .from('escena_dummy')
+      .from('progreso_usuario_juego')
       .select('*')
-      .eq('id_trabajador', req.params.id_trabajador)
+      .eq('id_usuario', req.params.id_usuario)
       .single();
 
       // If the curso is found, respond with the curso data
@@ -36,27 +36,33 @@ export const getEscena = async (req, res) => {
   }
 };
 
-export const putEscena = async (req, res) => {
+export const putEscenaYCurso = async (req, res) => {
+
+  var updates = {}
   try {
 
-    console.log('ID Trabajador:', req.params.id_trabajador);
-    console.log('Escena:', req.params.escena);
-
+    if (req.params.nuevo_curso == "no")
+    {
+      updates = { "id_escena": req.params.escena}
+    }
+    else{
+      updates= { "id_escena": req.params.escena, "id_curso":req.params.nuevo_curso, "documento":null}
+    }
 
     // Update data in the 'Cursos' table
+
     const { data, error } = await supabase
-    .from('escena_dummy')
-    .update({ "escena": req.params.escena })
-    .eq('id_trabajador', req.params.id_trabajador)
-    .select()
+      .from('progreso_usuario_juego')
+      .update(updates)
+      .eq('id_usuario', req.params.id_usuario)
+      .select();
 
     if (error) {
       console.error('Error updating escena:', error);
       throw error;
+    } else{
+      res.json(data);
     }
-
-    res.json(data);
-
 
   } catch (error) {
     res.status(500).json({ error: error.message });
--- api/routes/escenas.routes.js
+++ api/routes/escenas.routes.js
@@ -1,17 +1,17 @@
 import { Router } from "express";
 import {
   getEscena,
-  putEscena,
+  putEscenaYCurso,
   putCertificado
 } from "../controllers/escenas.controllers.js";
 
 const router = Router();
 
-router.get("/escena/:id_trabajador", getEscena);
+router.get("/escena/:id_usuario", getEscena);
 
-router.put("/put_escena/:id_trabajador/:escena", putEscena);
+router.put("/put_escena/:id_usuario/:escena/:nuevo_curso", putEscenaYCurso);
 
-router.put("/put_certificado/:id_trabajador", putCertificado);
+router.put("/put_certificado/:id_usuario", putCertificado);
 
 
 export default router;
