79f2f4e7909a7e5d03a88f313aa3f902048aa694
santivqzv <santiago.vqz@gmail.com>
2024-04-30 01:04:18 -0600

    Quedaron las KPI

--- neoris/src/components/Cards/courseCards.jsx
+++ neoris/src/components/Cards/courseCards.jsx
@@ -1,67 +1,56 @@
 import Card from '@mui/material/Card';
+import React, { useEffect, useState } from 'react';
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ');
-}
-
-const data = [
-  {
-    name: 'Unique visitors',
-    stat: '10,450',
-    change: '-12.5%',
-    changeType: 'negative',
-  },
-  {
-    name: 'Bounce rate',
-    stat: '56.1%',
-    change: '+1.8%',
-    changeType: 'positive',
-  },
-  {
-    name: 'Visit duration',
-    stat: '5.2min',
-    change: '+19.7%',
-    changeType: 'positive',
-  },
-];
-
-/*
-    { cantidad_nuevos_usuarios: 3, pais: 'México' },
-    { cantidad_nuevos_usuarios: 1, pais: 'Argentina' },
-    { cantidad_nuevos_usuarios: 1, pais: 'Mexico' },
-    { cantidad_nuevos_usuarios: 1, pais: 'Francia' },
-    { cantidad_nuevos_usuarios: 3, pais: 'España' },
-    { cantidad_nuevos_usuarios: 1, pais: 'Estados Unidos' }
-*/
-
-const CourseCards = ({data}) => {
-  console.log(data);
-  const nuevosUsuarios = data?.nuevos_usuarios;
-  const totalNuevosUsuarios = nuevosUsuarios.reduce((acc, cur) => acc + cur.cantidad_nuevos_usuarios, 0);
-  const label = nuevosUsuarios[0].pais ? nuevosUsuarios[0].pais : 'Global';
+const CourseCards = ({totalUsuarios, labelUsuarios, labelCurso, totalNuevosUsuarios, labelNuevosUsuarios, nombreCursoMasPopular}) => {
+  const [loading, setLoading] = useState(true);
 
   const dataTable = [
     {
-      label: label, cantidad_nuevos_usuarios: totalNuevosUsuarios
+      label: labelNuevosUsuarios, 
+      cantidad_nuevos_usuarios: totalNuevosUsuarios,
+      tipo: 'Nuevos Usuarios'
+    },
+    {
+      label: labelUsuarios, 
+      cantidad_nuevos_usuarios: totalUsuarios,
+      tipo: 'Usuarios'
+    },
+    {
+      label: labelCurso, 
+      cantidad_nuevos_usuarios: nombreCursoMasPopular,
+      tipo: 'Curso Mas Popular'
     }
   ];
+
+  useEffect(() => {
+    setLoading(true);
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 0);
+
+    return () => clearTimeout(timer); // Clear the timer if the component is unmounted
+  }, [totalUsuarios, labelUsuarios, labelCurso, totalNuevosUsuarios, labelNuevosUsuarios, nombreCursoMasPopular]);
+
+  if (loading) {
+    return <div>Loading...</div>; // Render a loading message while the dataTable is being set
+  }
+
   return (
-      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 pt-10">
-        {dataTable.map((item) => (
-          <Card key={item.label} className='p-3'>
-            <p className="text-base leading-5 font-medium text-gray-500" style={{textTransform: "capitalize"}}>
-              Usuarios {item.label}
+    <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 pt-10">
+      {dataTable.map((item) => (
+        <Card key={item.label} className='p-3'>
+          <p className="text-base leading-5 font-medium text-gray-500" style={{textTransform: "capitalize"}}>
+            {item.tipo} {item.label}
+          </p>
+          <div className="mt-2 flex items-baseline space-x-2.5">
+            <p className="text-2xl leading-9 font-semibold text-gray-900">
+              {item.cantidad_nuevos_usuarios}
             </p>
-            <div className="mt-2 flex items-baseline space-x-2.5">
-              <p className="text-2xl leading-9 font-semibold text-gray-900">
-                {item.cantidad_nuevos_usuarios}
-              </p>
-            </div>
-          </Card>
-        ))}
-      </div>
-
+          </div>
+        </Card>
+      ))}
+    </div>
   );
-}
+};
 
-export default CourseCards
+export default CourseCards;
--- neoris/src/pages/Admin/Home.jsx
+++ neoris/src/pages/Admin/Home.jsx
@@ -30,6 +30,18 @@ const Home = () => {
 
   console.log(data);
 
+  ////
+  const nuevosUsuarios = data?.nuevos_usuarios;
+  const usuarios = data?.total_usuarios;
+  const curso = data?.curso_mas_popular;
+  const totalUsuarios = usuarios[0].total_usuarios;
+  const labelUsuarios = usuarios[0].pais ? usuarios[0].pais : 'Global';
+  const labelCurso = curso[0].pais ? curso[0].pais : 'Global';
+  const totalNuevosUsuarios = nuevosUsuarios[0].cantidad_nuevos_usuarios;
+  const labelNuevosUsuarios = nuevosUsuarios[0].pais ? nuevosUsuarios[0].pais : 'Global';
+  const nombreCursoMasPopular = curso[0].nombre_curso ? curso[0].nombre_curso : 'Global';
+  ////
+
   return (
     <Box m="1.5rem 2.5rem">
       <FlexBetween>
@@ -57,7 +69,14 @@ const Home = () => {
           </Dialog>
         </Box>
       </FlexBetween>
-      <CourseCards data={data} className="p-20" />
+      <CourseCards 
+        totalUsuarios={totalUsuarios}
+        labelUsuarios={labelUsuarios}
+        labelCurso={labelCurso}
+        totalNuevosUsuarios={totalNuevosUsuarios}
+        labelNuevosUsuarios={labelNuevosUsuarios}
+        nombreCursoMasPopular={nombreCursoMasPopular}
+        className="p-20" />
       <div className="grid grid-cols-1 gap-6 xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-3 pt-6">
         <div className=" col-span-1 xs:col-span-1 sm:col-span-2 md:col-span-1 lg:col-span-1 h-[60vh]">
           <Pie />
