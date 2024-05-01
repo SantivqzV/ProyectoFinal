6e501beb847db9a9af78bf0af3bedc17d689322b
Elias Serrano <eliasjosf26@gmail.com>
2024-05-01 15:35:01 -0600

    token se pasa como parametro a userDashboard

--- api/controllers/dashboard.controller.js
+++ api/controllers/dashboard.controller.js
@@ -117,15 +117,9 @@ export const adminDashboardInfo = async (req,res) =>{
     }
 }
 
-
 export const userDashboardInfo = async (req, res) =>{
 
-    if (!req.cookies || !req.cookies.token) {
-        throw new Error('JWT cookie is missing');
-      }
-  
-    const token = req.cookies.token; // Assumes JWT token is stored in a cookie named 'jwt'
-    const decoded = token.decode(token);
+    const decoded = jwt.decode(req.params.token);
 
     var userInfoJson = {};
 
